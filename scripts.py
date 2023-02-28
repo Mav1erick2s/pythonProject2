d34dfa9c74cecbc60897f8ff8553570e9bae1867
Mav1erick2s <komoltsevaa1703@gmail.com>
2023-02-28 11:07:34 +0600

    Task 12.7.3, done

--- scripts.py
+++ scripts.py
@@ -3,10 +3,10 @@ money = int(input("Введите сумму:"))
 deposit=list(per_cent.values())
 
 def per_cent1(mon):
-    return mon*money
+    return mon*money/100
 deposit=(list(map(per_cent1, deposit)))
 
-max_deposit= max(deposit)
+max_deposit = max(deposit)
 print(deposit)
 print("Максимальная сумма, которую вы можете заработать - ", max_deposit)
 
