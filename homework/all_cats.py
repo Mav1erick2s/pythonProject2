b07a2269e0f4408a7aed36409e8541dea32842b1
Mav1erick2s <komoltsevaa1703@gmail.com>
2023-04-14 23:31:41 +0600

    hw16_7_1

---
+++ homework/all_cats.py
@@ -0,0 +1,7 @@
+from hw16_7_1 import Cat
+
+cat_baron = Cat('Барон', 'мальчик', '2')
+cat_sam = Cat('Сэм', 'мальчик', '2')
+
+print(f'Name: {cat_baron.name}\nGender: {cat_baron.gender}\nAge: {cat_baron.age}\n')
+print(f'Name: {cat_sam.name}\nGender: {cat_sam.gender}\nAge: {cat_sam.age}\n')
